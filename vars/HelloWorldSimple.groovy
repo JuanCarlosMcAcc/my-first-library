4d110a28ea9efe8739882e39074963ba4521fd64
JuanCarlosMcAcc <j.moratalla.campello@accenture.com>
2022-10-27 13:15:00 +0200

    multilineado

--- vars/HelloWorldSimple.groovy
+++ vars/HelloWorldSimple.groovy
@@ -1,4 +1,5 @@
 def call(String name, String apellido, String segundo_apellido) {
-    echo "Hello world, my name is ${name}, my first surname" +
-    " is ${apellido} and my last surname is ${segundo_apellido}."  
+    echo """Hello world, my name is ${name}, 
+    my first surname is ${apellido} 
+    and my last surname is ${segundo_apellido}."""  
 }
