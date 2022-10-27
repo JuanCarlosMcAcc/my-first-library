6512ab2a7d5e5817aa5d3b71b0e398d55479d346
JuanCarlosMcAcc <j.moratalla.campello@accenture.com>
2022-10-27 13:54:09 +0200

    dos call

--- vars/HelloWorldSimple.groovy
+++ vars/HelloWorldSimple.groovy
@@ -5,6 +5,6 @@ def call(String name, String surname, String second_surname) {
     """
 }
 
-funcion2(String parametro) {
+def call(String parametro) {
     echo """Esta es la funcion2 el valor del parametro es ${parametro}"""
 }
