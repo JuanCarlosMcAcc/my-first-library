1b76d84443a1fabf2b3d86d74a73d53f89a86db0
JuanCarlosMcAcc <j.moratalla.campello@accenture.com>
2022-10-27 12:24:42 +0200

    Nombre y apellidos

--- vars/HelloWorldSimple.groovy
+++ vars/HelloWorldSimple.groovy
@@ -1,3 +1,4 @@
-def call(String name, String apellido) {
-    echo "Hello world, ${name} ${apellido}."  
+def call(String name, String apellido, String segundo_apellido) {
+    echo "Hello world, my name is ${name}, my first surname
+    is ${apellido} and my last surname is ${segundo_apellido}."  
 }
