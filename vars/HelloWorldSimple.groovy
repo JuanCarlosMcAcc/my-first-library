01bd31170a8c88e800e90bc7b4624b1960869198
JuanCarlosMcAcc <j.moratalla.campello@accenture.com>
2022-10-27 12:36:02 +0200

    Solo un parametro

--- vars/HelloWorldSimple.groovy
+++ vars/HelloWorldSimple.groovy
@@ -1,4 +1,3 @@
 def call(String name, String apellido, String segundo_apellido) {
-    echo "Hello world, my name is ${name}, my first surname" +
-    "is ${apellido} and my last surname is ${segundo_apellido}."  
+    echo "Hello world, my name is ${name}"  
 }
