b9c0429b20e7e70d2afcae614ffad438f353a009
JuanCarlosMcAcc <j.moratalla.campello@accenture.com>
2022-10-27 13:29:30 +0200

    funcion1

--- vars/HelloWorldSimple.groovy
+++ vars/HelloWorldSimple.groovy
@@ -1,6 +1,6 @@
-def call(String name, String surname, String second_surname) {
+def funcion1(String name, String surname, String second_surname) {
     echo """    Hello world, my name is ${name}, 
     my first surname is ${surname} 
     and my last surname is ${second_surname}.
-    """  
+    """
 }
