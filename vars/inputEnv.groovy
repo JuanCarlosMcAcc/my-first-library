6d99a8aee3216224efdfd0b92f368277eed680c8
JuanCarlosMcAcc <j.moratalla.campello@accenture.com>
2022-10-31 13:02:27 +0100

    def

--- vars/inputEnv.groovy
+++ vars/inputEnv.groovy
@@ -1,4 +1,5 @@
 def call (){
+    def entorno = ''
         catchError(buildResult: 'SUCCESS', stageResult: 'SUCCESS', catchInterruptions: true) {
         timeout(time: 2, unit: 'MINUTES') {
             entorno = input(message: 'Elige el entorno de produccion',
