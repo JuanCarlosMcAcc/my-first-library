fc2359f9f17ffd33d39a2cf2de9b82786a01d68f
JuanCarlosMcAcc <j.moratalla.campello@accenture.com>
2022-10-31 11:54:41 +0100

    function attempt 2

--- vars/switchCaseVersion.groovy
+++ vars/switchCaseVersion.groovy
@@ -1,13 +1,3 @@
-def call(String version){
-    switch(version) {
-        case "0.0.5":
-            sh "grep 0.0.5 release.yml"
-            break
-        case "0.0.6":
-            sh "grep 0.0.6 release.yml"
-            break
-        case "0.0.7":
-            sh "grep 0.0.7 release.yml"
-            break
-    }
+def call(String entorno){   
+    sh ("grep ${entorno} release.yml")
 }
