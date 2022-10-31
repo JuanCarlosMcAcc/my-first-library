593f766f81ca72e8e68201dc674e18375abfd134
JuanCarlosMcAcc <j.moratalla.campello@accenture.com>
2022-10-31 11:28:13 +0100

    Intento nº1

--- vars/switchCaseVersion.groovy
+++ vars/switchCaseVersion.groovy
@@ -1,13 +1,13 @@
 def call(String version){
-    switch(${version}) {
+    switch(version) {
         case "0.0.5":
             sh "grep 0.0.5 release.yml"
             break
         case "0.0.6":
-            sh "grep ${version} release.yml"
+            sh "grep 0.0.6 release.yml"
             break
         case "0.0.7":
-            sh "grep ${version} release.yml"
+            sh "grep 0.0.7 release.yml"
             break
     }
 }
