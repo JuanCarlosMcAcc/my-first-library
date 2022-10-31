0f5d65998ed66791a4175ce63ea01dd73632d4ca
JuanCarlosMcAcc <j.moratalla.campello@accenture.com>
2022-10-31 12:38:12 +0100

    }

--- vars/inputEnv.groovy
+++ vars/inputEnv.groovy
@@ -1,12 +1,13 @@
 def call (String entorno){
-    catchError(buildResult: 'SUCCESS', stageResult: 'SUCCESS', catchInterruptions: true) {
-    timeout(time: 2, unit: 'MINUTES') {
-        entorno = input(message: 'Elige el entorno de produccion',
-            parameters: [
-            [$class : 'ChoiceParameterDefinition',
-            choices: ["INT", "PRE", "PRO"].join('\n'),
-            name   : 'Please, choose selection']
-        ])
+        catchError(buildResult: 'SUCCESS', stageResult: 'SUCCESS', catchInterruptions: true) {
+        timeout(time: 2, unit: 'MINUTES') {
+            entorno = input(message: 'Elige el entorno de produccion',
+                parameters: [
+                [$class : 'ChoiceParameterDefinition',
+                choices: ["INT", "PRE", "PRO"].join('\n'),
+                name   : 'Please, choose selection']
+            ])
+        }
+        return entorno
     }
-    return entorno
 }
