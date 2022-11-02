0d5b4a986a48321ce87cc7c36bb524935c5d31d4
JuanCarlosMcAcc <j.moratalla.campello@accenture.com>
2022-11-02 11:56:29 +0100

    |

--- vars/switchCaseVersion.groovy
+++ vars/switchCaseVersion.groovy
@@ -1,6 +1,6 @@
 def call(String entorno){
     // def resultado = new StringBuilder()
     // resultado = sh("grep ${entorno} release.yml")
-    echo "La version es " sh("grep ${entorno} release.yml")
+    echo "La version es " | sh("grep ${entorno} release.yml")
     // sh ("grep ${entorno} release.yml")
 }
