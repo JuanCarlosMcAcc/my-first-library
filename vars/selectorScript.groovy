c7609ff58e0b9aaa455a6237b3d5608fd8b20ca5
JuanCarlosMcAcc <j.moratalla.campello@accenture.com>
2022-11-11 13:27:21 +0100

    Reparacion

--- vars/selectorScript.groovy
+++ vars/selectorScript.groovy
@@ -1,11 +1,10 @@
 def call (String NOMBRE, CONTADOR){
         catchError(buildResult: 'SUCCESS', stageResult: 'SUCCESS', catchInterruptions: true) {
-        timeout(time: 15, unit: 'SECONDS') {
+        timeout(time: 2, unit: 'MINUTES') {
             script = input(message: 'Elige el script a ejecutar',
                 parameters: [
                 [$class : 'ChoiceParameterDefinition',
                 choices: ["Script Contador", "Script Nombre"].join('\n'),
-                defaultValue: "Script Contador",
                 name   : 'Please, choose selection']
             ])
         }
