8146ed78fff497d41c4c29d5e26e32c21b1d5ba4
JuanCarlosMcAcc <j.moratalla.campello@accenture.com>
2022-11-11 09:56:30 +0100

    scriptSelector

---
+++ vars/selectorScript.groovy
@@ -0,0 +1,13 @@
+def call (){
+        catchError(buildResult: 'SUCCESS', stageResult: 'SUCCESS', catchInterruptions: true) {
+        timeout(time: 2, unit: 'MINUTES') {
+            script = input(message: 'Elige el script a ejecutar',
+                parameters: [
+                [$class : 'ChoiceParameterDefinition',
+                choices: ["Script Contador", "Script Nombre"].join('\n'),
+                name   : 'Please, choose selection']
+            ])
+        }
+    }
+    return script
+}
--- vars/switchCaseVersion.groovy
+++ vars/switchCaseVersion.groovy
@@ -2,4 +2,5 @@ def call(String entorno){
 
     def resultado = sh(returnStdout: true, script: """ grep ${entorno} release.yml """)
     echo "La version es ${resultado} "
+    
 }
